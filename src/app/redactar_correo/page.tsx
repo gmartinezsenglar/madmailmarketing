9b11dea99fb196706355ca553c27811a35a60ca5
amolina20 <dbgthd413@gmail.com>
2025-06-07 23:05:04 -0400

    Lista de Clientes en Redactar Correo

--- src/app/redactar_correo/page.tsx
+++ src/app/redactar_correo/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { listaContactos } from "@/app/clientes_simulado"
+
 export default function RedactarCorreo() {
 
   return (
     <div className="flex flex-col min-h-screen text-black px-6 py-8">
-      <h1 className="text-center text-4xl font-bold mb-12 text-blue-950">
+      <h1 className="text-black text-2xl font-bold mb-4">
         Redactar Nuevo Correo
       </h1>
       <form className="flex flex-col">
@@ -12,7 +14,7 @@ export default function RedactarCorreo() {
             <label className="block text-lg mb-2">
                 Campaña
             </label>
-            <select  defaultValue="" className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700">
+            <select  required defaultValue="" className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700">
               <option value="" disabled >Selecciona una campaña</option>
               <option>Invierno 2025 - 20% Descuento</option>
               <option>Anuncio Nuevos Productos</option>
@@ -22,11 +24,12 @@ export default function RedactarCorreo() {
             <label className="block text-lg mb-2">
                 Lista de Contactos
             </label>
-            <select  defaultValue="" className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700">
+            <select required defaultValue="" className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700">
               <option value="" disabled >Selecciona una lista de contactos</option>
-              <option>Clientes Potenciales 2025</option>
-              <option>Inscritos Curso Marketing Digital</option>
-            </select>
+            {listaContactos.map((lista) => (
+              <option key={lista.id}>{lista.nombre}</option>         
+            ))}
+            </select>     
         </div>
         <div className="mb-6">
           <label className="block text-lg mb-2">
@@ -44,6 +47,7 @@ export default function RedactarCorreo() {
             Mensaje:
           </label>
           <textarea
+              required
               placeholder="Escribe aquí tu mensaje..."
               className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700 min-h-[190px]"
             />
