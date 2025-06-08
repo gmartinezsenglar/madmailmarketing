4f870bb742f5ea2c9be182203d7bbcadabc7bc5d
amolina20 <dbgthd413@gmail.com>
2025-06-07 22:58:47 -0400

    Datos Lista Clientes Simulado

---
+++ src/app/clientes_simulado.tsx
@@ -0,0 +1,33 @@
+export const listaContactos = [
+    {
+      id: "lista1",
+      nombre: "Clientes Premium",
+      contactos: [
+        { id: "contacto1", nombre: "Steve Carell", correo: "steve@gmail.com" },
+        { id: "contacto2", nombre: "Jon Hamm", correo: "jon@gmail.com" },
+        { id: "contacto3", nombre: "Kurt Cobain", correo: "kurtcobain@gmail.com" },
+        { id: "contacto4", nombre: "Damon Albarn", correo: "damonalbarn@gmail.com" },
+        
+      ],
+    },
+    {
+      id: "lista2",
+      nombre: "Clientes Potenciales",
+      contactos: [
+        { id: "contacto5", nombre: "Mark Zuckenberg", correo: "facebook@gmail.com" },
+        { id: "contacto6", nombre: "Coronel Sanders", correo: "sanders@gmail.com" },
+        { id: "contacto7", nombre: "Robert Smith", correo: "smithrobert@gmail.com" },
+        { id: "contacto8", nombre: "Leonardo Dicaprio", correo: "dicaprio@gmail.com" },
+      ],
+    },
+    {
+        id: "lista3",
+        nombre: "Estudiantes UCT",
+        contactos: [
+          { id: "contacto9", nombre: "Abdiel Molina", correo: "amolina@gmail.com" },
+          { id: "contacto10", nombre: "Gustavo Martinez", correo: "gmartinez@gmail.com" },
+          { id: "contacto11", nombre: "Matias Araya", correo: "maraya@gmail.com" },
+        ],
+      },
+  ]
+  
