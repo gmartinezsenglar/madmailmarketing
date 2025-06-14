284117dfb21aeea4d8e3b5cded56c219ddc91932
amolina20 <dbgthd413@gmail.com>
2025-06-14 19:35:08 -0400

    Archivo de Datos de  Campañas

---
+++ src/app/campanas.tsx
@@ -0,0 +1,47 @@
+export const campañas = [
+  {
+    id: "verano2024",
+    nombre: "Verano 2024",
+    correos: [
+      {
+        id: "1",
+        asunto: "Promoción Verano 2024",
+        contenido: "Contenido del correo 1...",
+        destinatarios: "clientes@ejemplo.com",
+        fecha: "2024-01-15",
+        enviados: 3,
+        recibidos: 2,
+        estado: "Enviado",
+        fechaEnvio: "2024-01-15",
+      },
+      {
+        id: "2",
+        asunto: "Nuevo producto disponible",
+        contenido: "Contenido del correo 2...",
+        destinatarios: "clientes@ejemplo.com",
+        enviados: 0,
+        recibidos: 0,
+        fecha: "2024-01-18",
+        estado: "Programado",
+        fechaEnvio: "2024-01-18",
+      },
+    ],
+  },
+  {
+    id: "invierno2024",
+    nombre: "Invierno 2024",
+    correos: [
+      {
+        id: "1",
+        asunto: "Oferta Invierno 2024",
+        contenido: "Contenido del correo invierno...",
+        destinatarios: "clientes@ejemplo.com",
+        fecha: "2024-06-10",
+        enviados: 0 ,
+        recibidos: 0,
+        estado: "Borrador",
+        fechaEnvio: "2024-06-10",
+      },
+    ],
+  },
+]
